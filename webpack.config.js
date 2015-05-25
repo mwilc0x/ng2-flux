cdaae5a1e992c2b8dc05944da928b8780f967c45
Mike Wilcox <mwilcox56@gmail.com>
2015-05-25 07:50:39 -0400

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,24 @@
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var path = require('path');
-var sliceArgs = Function.prototype.call.bind(Array.prototype.slice);
 
 module.exports = {
-  // devtool: 'source-maps',
-  devtool: 'eval',
-  devServer: {
-    inline: true,
-    colors: true,
-    historyApiFallback: true,
-    contentBase: 'public',
-    publicPath: '/__build__'
-  },
-
-  debug: true,
-  cache: true,
-
-  context: __dirname,
 
   entry: {
     angular2: [
-      // Angular 2 Deps
-      'zone.js',
-      //'zone.js/dist/long-stack-trace-zone.js',
       'reflect-metadata',
-      'rtts_assert/rtts_assert',
       './src/common/BrowserDomAdapter',
-      'angular2/angular2',
-      'angular2/router',
-      'angular2/di',
-      'angular2/src/facade/browser'
+      'angular2/angular2'
     ],
     app: [
-      // App
       './src/bootstrap'
     ]
   },
+
   output: {
-    path: 'public/__build__',
+    path: '/',
     filename: '[name].js',
-    // filename: '[name].[hash].js',
-    sourceMapFilename: '[name].js.map',
-    chunkFilename: '[id].chunk.js'
-    // publicPath: 'http://mycdn.com/'
+    contentBase: 'public/',
+    publicPath: '/public/'
   },
-
-
-  stats: {
-    colors: true,
-    reasons: true
-  },
-
-
 
   resolve: {
     root: __dirname,
@@ -60,44 +26,24 @@ module.exports = {
       '',
       '.js',
       '.ts',
-      // '.es6',
       '.json',
       '.webpack.js',
       '.web.js'
     ],
-    // Todo: learn more about aslias
-    alias: {
-      // When Angular2 has a TypeScript build
-      // 'angular2': grootNode('angular2/es6/prod'),
-
-      // 'app/*': '/app/*'
-      // 'components$': '/src/app/components/'
-      // 'decorators/*': '/app/decorators/*.js',
-      // 'services/*': '/app/services/*.js',
-      // 'stores/*': '/app/stores/*.js'
-      // 'angular2': 'angular2/es6/dev'
-    },
     modulesDirectories: [
       'web_modules',
       'node_modules',
-      'src' // hard coded for now until I figure out alias
+      'src'
     ]
   },
 
   module: {
     loaders: [
-      // Support for *.json files.
-      { test: /\.json$/,                    loader: 'json' },
-      // Support for CSS
-      { test: /\.css$/,                     loader: 'raw' },
-      // Copy all assets in to asset folder (use hash filename)
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.css$/, loader: 'raw' },
       { test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/, loader: 'file?name=assets/[hash].[ext]' },
-      // Copy all .html as static file (keep filename)
-      // { test: /index[a-z-]*\.html$/,        loader: 'file?name=[path][ ].html&context=./src' },
-      // support for .html as static file
-      { test: /\.html$/,                    loader: 'raw' },
-      // Support for .ts files.
-      { test: /\.ts$/,                      loader: 'typescript-simple' }
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.ts$/, loader: 'typescript-simple' }
     ],
     noParse: [
       /rtts_assert\/src\/rtts_assert/
@@ -113,41 +59,8 @@ module.exports = {
       name: 'common',
       filename: 'common.js'
     }),
-    new webpack.DefinePlugin({
-      'ENV': {
-        'type': JSON.stringify('development'),
-        'debug': true
-      }
-    }),
-    // new HtmlWebpackPlugin({
-    //   inject: true,
-    //   template: './src/index.html',
-    //   title: getBanner(),
-    //   filename: '../index.html',
-    //   chunks: ['shared']
-    // }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     drop_debugger: false
-    //   }
-    // }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.BannerPlugin(getBanner())
+    new webpack.optimize.DedupePlugin()
   ]
 
 };
-
-function getBanner() {
-  return 'ng2 webpack';
-}
-
-function groot(args) {
-  args = sliceArgs(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
-}
-function grootNode(args) {
-  args = sliceArgs(arguments, 0);
-  return groot.apply(path, ['node_modules'].concat(args));
-}
