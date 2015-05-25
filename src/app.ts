2ee592ad5828de5c5e5ae06036fef0e0d332f963
Mike Wilcox <mwilcox56@gmail.com>
2015-05-25 07:49:18 -0400

    add routing

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,31 @@
 import {bootstrap, Component, View} from 'angular2/angular2';
 import {RouteConfig, RouterOutlet, RouterLink} from 'angular2/router';
 import {bind} from 'angular2/di';
+import {Header} from './components/header/header';
 import {Home} from './components/home/home';
+import {News} from './components/news/news';
 
 @Component({
 	selector:'app'
 })
 @View({
-	directives: [ RouterOutlet, RouterLink, Home ],
+	directives: [ RouterOutlet, RouterLink, Home, Header ],
 	template: `
 
-		<h1>NY Times Best Sellers Lists</h1>
+		<header></header>
 
-		<main>
-			<router-outlet></router-outlet>
-		</main>
+		<div class="container home">
+
+			<main>
+				<router-outlet></router-outlet>
+			</main>
+
+		</div>
 
 	`,
 })
 @RouteConfig([
   { path: '/', as: 'home', component: Home },
+	{ path: '/news', as: 'news', component: News }
 ])
 export class App { }
