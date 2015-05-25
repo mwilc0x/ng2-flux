57183e223bb7b2ae84b4d8e9010f9905caa80e96
Mike Wilcox <mwilcox56@gmail.com>
2015-05-25 07:46:27 -0400

    add a loader

--- src/components/book-list/book-list.ts
+++ src/components/book-list/book-list.ts
@@ -1,7 +1,8 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-import {Component, View, NgFor} from 'angular2/angular2';
+import {Component, View, NgFor, NgIf} from 'angular2/angular2';
 import {Book} from '../book/book';
+import {Loader} from '../loader/loader'
 
 @Component({
     selector: 'book-list',
@@ -10,8 +11,11 @@ import {Book} from '../book/book';
     }
 })
 @View({
-    directives: [NgFor, Book],
+    directives: [NgIf, NgFor, Book, Loader],
     template: `
+        <div *ng-if="!lists.length">
+          <loader></loader>
+        </div>
         <div *ng-for="#list of lists">
             <h2>{{list.title}}</h2>
             <div *ng-for="#book of list.books">
