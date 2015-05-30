67eea1795a7d39c5626d7362e67410fe6904fa55
Mike Wilcox <mwilcox56@gmail.com>
2015-05-30 14:02:42 -0400

    hook-in flux recorder to the main fb dispatcher

--- src/utils/dispatcher.ts
+++ src/utils/dispatcher.ts
@@ -1,9 +1,26 @@
 import * as flux from 'flux';
+var FluxRecorder = require('./flux-recorder');
 
 class Dispatcher extends flux.Dispatcher<any> {
 
+  private _recorder: any;
+
   constructor() {
     super();
+
+    this._recorder = new FluxRecorder(this);
+  }
+
+  startRecording() {
+    this._recorder.startRecording();
+  }
+
+  stopRecording() {
+    this._recorder.stopRecording();
+  }
+
+  listenToHotKeys() {
+    this._recorder.listenToHotKeys();
   }
 
 }
